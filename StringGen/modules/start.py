48f97fbc8d5476bacbe9607526ec2c2bcabe2986
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-12-13 14:04:07 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -19,18 +19,7 @@ async def f_start(_, message: Message):
 @Anony.on_message(filters.command("help") & filters.private & filters.incoming)
 async def f_help(_, message: Message):
     await message.reply_text(
-        text=f"""
-        ✅ 𝗦𝘁𝗲𝗽𝘀 𝘁𝗼 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲 𝗦𝗲𝘀𝘀𝗶𝗼𝗻 𝗦𝘁𝗿𝗶𝗻𝗴
-
- ▪️ ꜱᴇꜱꜱɪᴏɴ ꜱᴛʀɪɴɢ ɪꜱ ɴᴇᴇᴅ ꜰᴏʀ ᴜꜱᴇʀʙᴏᴛꜱ, ɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴇꜱꜱɪᴏɴ ꜱᴛʀɪɴɢ ꜰᴏʀ ʙᴏᴛʜ ᴘʏʀᴏɢʀᴀᴍ & ᴛᴇʟᴇᴛʜᴏɴ.
-
- ▪️ ꜱᴇɴᴅ /start ᴛᴏ ꜱᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ ꜱᴇꜱꜱɪᴏɴ ꜱᴛʀɪɴɢ.
-
-1. ɪ ɴᴇᴇᴅꜱ API_ID, API_HASH (ʙᴏᴛʜ ᴄᴀɴ ʙᴇ ɢᴇᴛ ꜰʀᴏᴍ my.telegram.org), ᴘʜᴏɴᴇ ɴᴜᴍʙᴇʀ ᴀɴᴅ ᴏɴᴇ ᴛɪᴍᴇ ᴠᴇʀɪꜰɪᴄᴀᴛɪᴏɴ ᴄᴏᴅᴇ ᴡʜɪᴄʜ ᴡɪʟʟ ʙᴇ ꜱᴇɴᴛ ᴛᴏ ʏᴏᴜʀ ᴘʜᴏɴᴇ ɴᴜᴍʙᴇʀ.
-
-2. ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴘᴜᴛ ᴏᴛᴘ ɪɴ 1 2 3 4 5 6 ᴛʜɪꜱ ꜰᴏʀᴍᴀᴛ.
-
-3. ɪꜰ ᴛꜰᴀ ᴇɴᴀʙʟᴇᴅ ɪɴ ʏᴏᴜʀ ᴀᴄᴄᴏᴜɴᴛ, ʙᴏᴛ ᴡɪʟʟ ᴀʟꜱᴏ ᴀꜱᴋ ᴛʜᴀᴛ ꜰᴏʀ ɢᴇɴᴇʀᴀᴛɪɴɢ ꜱᴇꜱꜱɪᴏɴ ꜱᴛʀɪɴɢ ᴘᴜʀᴘᴏꜱᴇ ᴏɴʟʏ.      """,
+        text=f" help baby",
         reply_markup=keyboard2,
         disable_web_page_preview=True,
     )
