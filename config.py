582af07c2b2f9f094a303590df45daacf5a10154
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-12-14 16:11:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,6 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 2101893551))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/GJ516_DISCUSS_GROUP")
-Must_JOIN = getenv("MUST_JOIN","https://t.me/GJ516_DISCUSS_GROUP") 
+
+'''
+Must_JOIN = getenv("MUST_JOIN","https://t.me/GJ516_DISCUSS_GROUP") '''
