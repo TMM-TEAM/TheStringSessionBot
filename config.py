6f42e7ffefc514b27612a745c7c39f148f9d9172
TMM-TEAM <150228624+TMM-TEAM@users.noreply.github.com>
2024-04-23 14:23:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 2101893551))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ThinkGrowMore")
+OWNER_ID = int(getenv("OWNER_ID", 5311223486))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tmm_support_chat")
 
 '''
-Must_JOIN = getenv("MUST_JOIN","https://t.me/GJ516_DISCUSS_GROUP") '''
+Must_JOIN = getenv("MUST_JOIN","https://t.me/tmm_heroku_world") '''
