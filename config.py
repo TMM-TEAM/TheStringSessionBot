83c9d899309bd637c4be2d196548f21e733639f0
TMM-TEAM <150228624+TMM-TEAM@users.noreply.github.com>
2024-04-28 10:37:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 OWNER_ID = int(getenv("OWNER_ID", 5311223486))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tmm_support_chat")
 
-'''
-Must_JOIN = getenv("MUST_JOIN","https://t.me/tmm_heroku_world") '''
+
+Must_JOIN = getenv("MUST_JOIN","https://t.me/tmm_heroku_world") 
