0cb56a8dc527a848a0a3e546ebd6c36f4719d286
TMM-TEAM <150228624+TMM-TEAM@users.noreply.github.com>
2024-05-01 20:03:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ OWNER_ID = int(getenv("OWNER_ID", 5311223486))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tmm_support_chat")
 
 
-Must_JOIN = getenv("MUST_JOIN","tmm_heroku_world") 
+Must_JOIN = getenv("MUST_JOIN","https://t.me/tmm_heroku_world") 
